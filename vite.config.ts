0b11a56ff7b62b7e6a6a5020993fc71178e757fe
MorozovSergey81 <srmorozov@edu.hse.ru>
2025-06-08 17:38:02 +0500

    Configure for Vercel deployment

--- vite.config.ts
+++ vite.config.ts
@@ -2,6 +2,7 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react-swc'
 import path from 'path'
 
+// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
   resolve: {
@@ -9,5 +10,6 @@ export default defineConfig({
       '@': path.resolve(__dirname, './src'),
     },
   },
-  base: '/Harmony/',
+  // Remove the base for Vercel deployment
+  base: '/',
 }) 
